33fe55d0ebe9e3dc9c115bd3b480eb95d7530b17
Nate <github email address>
2019-01-30 18:43:12 -0500

    labs passed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :name
+  attr_accessor :roster, :grade
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_by_name = {}
+    @roster.each do |grade, student_arr|
+      sorted_by_name[grade] = student_arr.sort
+      #binding.pry
+    end
+    sorted_by_name
+  end
+
+end
+
+# flatiron = School.new("Flatiron School")
+
+# flatiron.add_student("Jay", 15)
